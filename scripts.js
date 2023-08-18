3138431bd1b28594035bb9c864053419719582c8
Haxaru <sammyalanis43@gmail.com>
2023-08-17 18:56:27 -0700

    Organize code and add reset button

--- scripts.js
+++ scripts.js
@@ -1,55 +1,66 @@
-function getComputerChoice() {
-  const computerChoice = ["rock", "paper", "scissors"];
-
-  return computerChoice[Math.floor(Math.random() * computerChoice.length)];
-}
-
-// The above generates the computer choice and logs it
+const results = document.querySelector(".results");
+const playerDisplay = document.querySelector(".player");
+const computerDisplay = document.querySelector(".computer");
 
 let playerPoints = 0;
 let computerPoints = 0;
 let totalRounds = 0;
 
-// The above is the starting points for the game
+const rockBtn = document.querySelector(".rock");
+rockBtn.addEventListener("click", () => {
+  playRound("rock");
+});
 
-const results = document.querySelector(".results");
-const playerDisplay = document.querySelector(".player");
-const computerDisplay = document.querySelector(".computer");
+const paperBtn = document.querySelector(".paper");
+paperBtn.addEventListener("click", () => {
+  playRound("paper");
+});
 
-// The linking to the classes
+const scissorsBtn = document.querySelector(".scissors");
+scissorsBtn.addEventListener("click", () => {
+  playRound("scissors");
+});
 
-function playRound(playerSelection, computerSelection) {
-  playerSelection;
-  computerSelection = getComputerChoice();
+const resetBtn = document.querySelector(".reset");
+resetBtn.addEventListener("click", reset);
 
-  if (playerSelection == "rock" && computerSelection === "paper") {
-    computerPoints++;
-    results.textContent = "You lose! Paper beats Rock!";
-  } else if (playerSelection == "rock" && computerSelection === "scissors") {
-    playerPoints++;
-    results.textContent = "You win! Rock beats Scissors!";
-  } else if (playerSelection === "paper" && computerSelection === "scissors") {
-    computerPoints++;
-    results.textContent = "You lose! Scissors beats Paper!";
-  } else if (playerSelection === "paper" && computerSelection === "rock") {
-    playerPoints++;
-    results.textContent = "You win! Paper beats Rock!";
-  } else if (playerSelection === "scissors" && computerSelection === "rock") {
-    computerPoints++;
-    results.textContent = "You lose! Rock beats Scissors!";
-  } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    playerPoints++;
-    results.textContent = "You win! Scissors beats Paper!";
-  } else if (playerSelection === computerSelection) {
+const computerChoice = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+  return computerChoice[Math.floor(Math.random() * computerChoice.length)];
+}
+
+function playRound(playerSelection) {
+  const computerSelection = getComputerChoice();
+
+  if (playerSelection === computerSelection) {
     results.textContent = "It's a tie!";
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    playerPoints++;
+    results.textContent = `You win! ${playerSelection} beats ${computerSelection}!`;
   } else {
-    results.textContent = "Input not recognized! Defaulting to a tie!";
+    computerPoints++;
+    results.textContent = `You lose! ${computerSelection} beats ${playerSelection}!`;
   }
   totalRounds++;
+  updatePoints();
+  declareWinner();
 
+  if (playerPoints > 5 || computerPoints > 5) {
+    reset();
+  }
+}
+
+function updatePoints() {
   playerDisplay.textContent = playerPoints;
   computerDisplay.textContent = computerPoints;
+}
 
+function declareWinner() {
   if (playerPoints === 5) {
     results.textContent = `Yay! You win with a score of ${playerPoints} to ${computerPoints}!`;
   } else if (computerPoints === 5) {
@@ -57,21 +68,10 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-// The above relays the outcome of the game.
-
-const rock = document.querySelector(".rock");
-rock.addEventListener("click", () => {
-  playRound("rock");
-});
-
-const paper = document.querySelector(".paper");
-paper.addEventListener("click", () => {
-  playRound("paper");
-});
-
-const scissors = document.querySelector(".scissors");
-scissors.addEventListener("click", () => {
-  playRound("scissors");
-});
-
-// The buttons representing the player selection
+function reset() {
+  playerPoints = 0;
+  computerPoints = 0;
+  playerDisplay.textContent = playerPoints;
+  computerDisplay.textContent = computerPoints;
+  results.textContent = "";
+}
